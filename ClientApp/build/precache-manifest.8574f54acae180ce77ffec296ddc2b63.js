49bac8064cc13db03bbba0335cb8279351f6c26f
Senthilnathan Rajakumar <113014077+SenthilnathanRajakumar@users.noreply.github.com>
2023-01-20 00:28:46 +0800

    v1.0

---
+++ ClientApp/build/precache-manifest.8574f54acae180ce77ffec296ddc2b63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab0e443d04dbb548b28fcece675b1ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e75ed285552529d05f",
+    "url": "/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "2271bd3741f388d97275",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "79e75ed285552529d05f",
+    "url": "/static/js/2.9a38f799.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.9a38f799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2271bd3741f388d97275",
+    "url": "/static/js/main.2e12bdfb.chunk.js"
+  },
+  {
+    "revision": "81b83c328c9497c86d6d",
+    "url": "/static/js/runtime-main.9b5c6c1f.js"
+  }
+]);
